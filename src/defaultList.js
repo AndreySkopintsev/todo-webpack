30823fd8b394e823b433eac4d415950279c89347
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2021-10-19 12:58:59 +0300

    added date input

--- src/defaultList.js
+++ src/defaultList.js
@@ -1,6 +1,6 @@
 // Factory function for list objects
-const ListObject = (name) =>{
-    return {name}
+const ListObject = (name,date) =>{
+    return {name,date}
 }
 
 // Elements of default list
@@ -14,8 +14,15 @@ inputBtn.textContent = 'New item'
 const input = document.createElement('input')
 input.setAttribute('id','newItem')
 
+
+const inputDate = document.createElement('input')
+inputDate.setAttribute('type','date')
+
 inputBox.appendChild(input)
+inputBox.appendChild(inputDate)
 inputBox.appendChild(inputBtn)
+
+const listContainer = document.createElement('ul')
 
 const div = document.createElement('div')
 div.classList.add('listBox')
@@ -24,14 +31,23 @@ div.classList.add('listBox')
 function defaultList(){
     div.appendChild(h1)
     div.appendChild(inputBox)
+    div.appendChild(listContainer)
+
     return div
 }
 
 // Event listeners
 inputBtn.addEventListener('click',()=>{
     if(input.value){
-        let newItem = ListObject(input.value) 
+        let newItem = ListObject(input.value,inputDate.value) 
+        let listEl = document.createElement('li')
+        let p = document.createElement('p')
+        p.textContent = newItem.name
+        listEl.appendChild(p)
         console.log(newItem)
+
+        listContainer.appendChild(listEl)
+        input.value = ''
     }else{
         console.log('invalid value')
     }
