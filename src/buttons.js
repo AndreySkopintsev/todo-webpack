43bc38a1a0a5afe2b856ddac1b1c243da488f1a8
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2021-10-27 11:37:33 +0300

    installed prettier and tested on a couple of modules

--- src/buttons.js
+++ src/buttons.js
@@ -1,42 +1,42 @@
-import defaultList from './defaultList'
-import todayList from './todayList'
-import weekList from './weekList'
+import defaultList from "./defaultList";
+import todayList from "./todayList";
+import weekList from "./weekList";
 
-function inboxBtn(){
-    const section = document.querySelector('section')
-    const button = document.createElement('button')
-    button.setAttribute('id','inboxBtn')
-    button.textContent = 'Inbox'
-    button.addEventListener('click',()=>{
-        section.innerHTML = ''
-        section.appendChild(defaultList())
-    })
-    
-    return button
+function inboxBtn() {
+  const section = document.querySelector("section");
+  const button = document.createElement("button");
+  button.setAttribute("id", "inboxBtn");
+  button.textContent = "Inbox";
+  button.addEventListener("click", () => {
+    section.innerHTML = "";
+    section.appendChild(defaultList());
+  });
+
+  return button;
 }
 
-function todayBtn(){
-    const section = document.querySelector('section')
-    const button = document.createElement('button')
-    button.textContent = 'Today'
-    button.addEventListener('click',()=>{
-        section.innerHTML = ''
-        section.appendChild(todayList())
-    })
-    
-    return button
+function todayBtn() {
+  const section = document.querySelector("section");
+  const button = document.createElement("button");
+  button.textContent = "Today";
+  button.addEventListener("click", () => {
+    section.innerHTML = "";
+    section.appendChild(todayList());
+  });
+
+  return button;
 }
 
-function weekBtn(){
-    const section = document.querySelector('section')
-    const button = document.createElement('button')
-    button.textContent = 'This week'
-    button.addEventListener('click',()=>{
-        section.innerHTML = ''
-        section.appendChild(weekList())
-    })
-    
-    return button
+function weekBtn() {
+  const section = document.querySelector("section");
+  const button = document.createElement("button");
+  button.textContent = "This week";
+  button.addEventListener("click", () => {
+    section.innerHTML = "";
+    section.appendChild(weekList());
+  });
+
+  return button;
 }
 
-export {inboxBtn,todayBtn,weekBtn}
+export { inboxBtn, todayBtn, weekBtn };
--- src/defaultList.js
+++ src/defaultList.js
@@ -1,56 +1,65 @@
 // Factory function for list objects
-const ListObject = (name,date) =>{
-    return {name,date}
-}
+const ListObject = (name, date) => {
+  return { name, date };
+};
 
 // Elements of default list
-const h1 = document.createElement('h1')
-h1.textContent = 'Default List'
+const h1 = document.createElement("h1");
+h1.textContent = "Default List";
 
-const inputBox = document.createElement('div')
-inputBox.classList.add('inputBox')
-const inputBtn = document.createElement('button')
-inputBtn.textContent = 'New item'
-const input = document.createElement('input')
-input.setAttribute('id','newItem')
+const inputBox = document.createElement("div");
+inputBox.classList.add("inputBox");
+const inputBtn = document.createElement("button");
+inputBtn.innerHTML = '<i class="fas fa-plus"></i> Add new item';
+const input = document.createElement("input");
+input.setAttribute("id", "newItem");
+const submitBtn = document.createElement("button");
+submitBtn.textContent = "Submit";
 
+const inputDate = document.createElement("input");
+inputDate.setAttribute("type", "date");
 
-const inputDate = document.createElement('input')
-inputDate.setAttribute('type','date')
+inputBox.appendChild(input);
+inputBox.appendChild(inputDate);
+inputBox.appendChild(submitBtn);
+inputBox.classList.add("hidden", "inputBox");
 
-inputBox.appendChild(input)
-inputBox.appendChild(inputDate)
-inputBox.appendChild(inputBtn)
+const listContainer = document.createElement("ul");
 
-const listContainer = document.createElement('ul')
+const div = document.createElement("div");
+div.classList.add("listBox");
 
-const div = document.createElement('div')
-div.classList.add('listBox')
+function defaultList() {
+  div.appendChild(h1);
+  div.appendChild(inputBox);
+  div.appendChild(inputBtn);
+  div.appendChild(listContainer);
 
-
-function defaultList(){
-    div.appendChild(h1)
-    div.appendChild(inputBox)
-    div.appendChild(listContainer)
-
-    return div
+  return div;
 }
 
 // Event listeners
-inputBtn.addEventListener('click',()=>{
-    if(input.value){
-        let newItem = ListObject(input.value,inputDate.value) 
-        let listEl = document.createElement('li')
-        let p = document.createElement('p')
-        p.textContent = newItem.name
-        listEl.appendChild(p)
-        console.log(newItem)
+inputBtn.addEventListener("click", () => {
+  inputBtn.classList.add("hidden");
+  inputBox.classList.remove("hidden");
+});
 
-        listContainer.appendChild(listEl)
-        input.value = ''
-    }else{
-        console.log('invalid value')
-    }
-})
+submitBtn.addEventListener("click", () => {
+  if (input.value) {
+    let newItem = ListObject(input.value, inputDate.value);
+    let listEl = document.createElement("li");
+    let p = document.createElement("p");
+    p.textContent = newItem.name;
+    listEl.appendChild(p);
+    console.log(newItem);
 
-export default defaultList
+    listContainer.appendChild(listEl);
+    inputBox.classList.add("hidden");
+    inputBtn.classList.remove("hidden");
+    input.value = "";
+  } else {
+    console.log("invalid value");
+  }
+});
+
+export default defaultList;
